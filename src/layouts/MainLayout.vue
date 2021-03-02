4a6acd86b506d55da2470f032bc071a4b50da42d
Katrin Taltseva <talzeva@yandex.ru>
2021-03-02 11:46:53 +0400

    add router guard, MainLayout handler error

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -23,6 +23,7 @@
 <script>
 import Navbar from "@/components/app/Navbar";
 import Sidebar from "@/components/app/Sidebar";
+import messages from "@/utils/messages";
 
 export default {
   data: () => ({
@@ -34,6 +35,16 @@ export default {
       await this.$store.dispatch('fetchInfo')
     }
     this.loading = false
+  },
+  computed: {
+    error() {
+      return this.$store.getters.error
+    }
+  },
+  watch: {
+    error(fbError) {
+      this.$error(messages[fbError.code] || 'Что-то пошло не так')
+    }
   },
   components: {
     Navbar, Sidebar
